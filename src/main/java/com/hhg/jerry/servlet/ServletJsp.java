a7b161de911bc9979cb90ee8c671deeaaba9d3ae
lining01 <lining01@xiaoshouyi.com>
2018-09-06 12:56:12 +0800

    return jsp for servlet

---
+++ src/main/java/com/hhg/jerry/servlet/ServletJsp.java
@@ -0,0 +1,25 @@
+package com.hhg.jerry.servlet;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+/**
+ * Created by lining on 2018/7/13.
+ */
+public class ServletJsp extends HttpServlet{
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("name","守涛");
+        req.getRequestDispatcher("WEB-INF/test.jsp").forward(req, resp);
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
---
+++ src/main/webapp/WEB-INF/test.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>test for jsp</h1>
+<p>你好，${name}</p>
+</body>
+</html>
